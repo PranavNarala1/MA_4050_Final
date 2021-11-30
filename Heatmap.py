a48ff796e7bc10358cdce7718d3d8645c2b5e5e9
PranavNarala1 <56051957+PranavNarala1@users.noreply.github.com>
2021-11-30 11:51:10 -0500

    Starting working on Heatmap.py

---
+++ Heatmap.py
@@ -0,0 +1,10 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+#from equationGenerator import getLambda
+
+states = np.arange(0, 101, 1)
+
+print(states)
+
+#getLambda(state_parameter)
