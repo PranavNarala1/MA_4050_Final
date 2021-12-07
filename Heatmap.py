c2b4a582eee3c1d1faf5f33dbee2f3bf4d6ccef3
PranavNarala1 <56051957+PranavNarala1@users.noreply.github.com>
2021-12-07 15:17:03 -0500

    Working on Heatmap.py

--- Heatmap.py
+++ Heatmap.py
@@ -1,9 +1,11 @@
+#imports
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import equationGenerator
 import seaborn as sns
 
+#defining getHeatMap
 def getHeatMap(turn):
     states_dataframe = equationGenerator.genLambdas()
     row_value = turn / 0.01
@@ -13,6 +15,27 @@ def getHeatMap(turn):
     for x in [1, 3, 5, 7, 9]:
         state_values[x] = state_values[x][::-1]
 
+    state_values = np.flip(state_values, 0)
     heat_map = sns.heatmap(state_values)
 
+getHeatMap(1)
+getHeatMap(5)
 getHeatMap(10)
+getHeatMap(15)
+getHeatMap(20)
+getHeatMap(25)
+getHeatMap(30)
+getHeatMap(35)
+getHeatMap(40)
+getHeatMap(45)
+getHeatMap(50)
+getHeatMap(55)
+getHeatMap(60)
+getHeatMap(65)
+getHeatMap(70)
+getHeatMap(75)
+getHeatMap(80)
+getHeatMap(85)
+getHeatMap(90)
+getHeatMap(95)
+getHeatMap(100)
